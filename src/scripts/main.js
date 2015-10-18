19a4295666423a1b8cf49880253b1911236874fe
YUCHAO WU <yuchao.sydneyuni@gmail.com>
2015-10-18 16:12:28 +1100

    finished desktop version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 var test = 1;
+
+function initMap() {
+  var myLatLng = {lat: -33.889218, lng: 151.204917};
+  var centerLatLng = {lat: -33.8877994, lng: 151.1802392};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 14,
+    center: centerLatLng,
+    scrollwheel:  false
+  });
+  
+  var iconBase = 'http://localhost:3000/images/';
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Hello World!',
+    icon: iconBase + 'map_icon.png'
+  });
+}
