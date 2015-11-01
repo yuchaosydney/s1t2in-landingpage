0f8cb818650fe3b9549d0ce5d12da99f2d29d299
YUCHAO WU <yuchao.sydneyuni@gmail.com>
2015-11-01 23:14:06 +1100

    made changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,8 @@
 var test = 1;
+var animate_time = 200;
+var is_form_show = false;
+var break_point = 768;
+var is_small_screen = $(window).width() < break_point;
 
 function initMap() {
   var myLatLng = {lat: -33.889218, lng: 151.204917};
@@ -18,3 +22,50 @@ function initMap() {
     icon: iconBase + 'map_icon.png'
   });
 }
+
+/**
+  *call contact form click event function
+  **/
+show_contact_form = function() {
+  $('#pageslide').animate(
+    {
+      "display": "block",
+      "right": "0px"
+    }, animate_time);
+
+    $('body').animate(
+      {
+        "margin-left": "-450px"
+      },animate_time);
+}
+/**
+  *hide contact form click event function
+  **/
+hide_contact_form = function() {
+  $('#pageslide').animate(
+    {
+      "display": "block",
+      "right": "-450px"
+    }, animate_time);
+
+    $('body').animate(
+    {
+      "margin-left": "0px"
+    },animate_time);
+}
+
+      
+$(".button").on("click", function( e ) {
+
+  e.preventDefault();
+  if(is_form_show) {
+    //form is showing, need hide
+    hide_contact_form();
+    is_form_show = false;
+  } else {
+    //form is hiding, need show
+    show_contact_form();
+    is_form_show = true;
+  }
+
+});
